bacc750cb05a7c2f248787b3777b5e0871ff82d8
oceanroleplay <indianoceanroleplay@gmail.com>
2022-01-05 10:29:37 +0530

    fix: project structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Document } from "domhandler";
 import axios, { AxiosResponse } from "axios";
-import { DomUtils, parseDocument } from "htmlparser2";
+import parser, { DomUtils } from "htmlparser2";
 import axiosRetry from "axios-retry";
 import cheerio from "cheerio";
 
@@ -8,33 +8,34 @@ import cheerio from "cheerio";
 axiosRetry(axios, { retries: 5 });
 
 export interface RawApplePlaylist {
-    name: string;
-    type: "playlist" | "album";
-    author: string;
-    tracks: tracks[];
+  name: string;
+  type: "playlist" | "album";
+  author: string;
+  tracks: Track[];
 }
+
 export interface RawAppleSong {
-    type?: "song";
-    artist: string;
-    title: string;
-    album: string;
+  type?: "song";
+  artist: string;
+  title: string;
+  album: string;
 }
 
-interface tracks {
-    artist: string; 
-    title: string; 
-    album: string
-};
-
-interface HalfSong {
-    artist: string;
-    album: string;
+export interface Track {
+  artist: string;
+  title: string;
+  album: string;
 }
 
-export interface HalfPlaylist {
-    name: string;
-    type: "playlist";
-    author: string;
+export interface PartialSong {
+  artist: string;
+  album: string;
+}
+
+export interface PartialPlaylist {
+  name: string;
+  type: "playlist";
+  author: string;
 }
 
 /**
@@ -43,149 +44,146 @@ export interface HalfPlaylist {
  * @returns {Promise<?RawApplePlaylist>}
  */
 export async function findJSONLD(
-    document: Document,
-    album: boolean = false,
-    fast: boolean = false,
-): Promise<RawApplePlaylist | HalfSong | HalfPlaylist | undefined> {
-    const scripts = DomUtils.findAll((element) => {
-        if (element.type !== "script") return false;
+  document: Document,
+  album: boolean = false,
+  fast: boolean = false
+): Promise<RawApplePlaylist | PartialSong | PartialPlaylist | undefined> {
+  const scripts = DomUtils.findAll((element) => {
+    if (element.type !== "script") return false;
 
-        return element.attribs.type === "application/ld+json";
-    }, document.children);
+    return element.attribs.type === "application/ld+json";
+  }, document.children);
 
-    for (const script of scripts) {
-        let data = JSON.parse(DomUtils.textContent(script));
-        if ("@graph" in data) data = data["@graph"];
-        if (data["@type"] === "MusicAlbum" && !album) {
-            return {
-                artist: data.byArtist.name as string,
-                album: data.name as string,
-            };
-        }
-        if (data["@type"] === "MusicAlbum" && album) {
-            let { name, byArtist, tracks } = data;
-
-            return {
-                type: "album",
-                name: name as string,
-                author: byArtist.name as string,
-                tracks: tracks.map((songData: any) => {
-                    return {
-                        artist: byArtist.name as string,
-                        title: songData.name as string,
-                    };
-                }),
-            };
-        }
-        if (data["@type"] === "MusicPlaylist" && fast) {
-            let { name, author } = data;
-            return {
-                type: "playlist",
-                name: name as string,
-                author: author.name as string
-            }
-        }
-        if (data["@type"] === "MusicPlaylist") {
-            let { name, author, track } = data;
-            return {
-                type: "playlist",
-                name: name as string,
-                author: author.name as string,
-                tracks: (await Promise.all(
-                    track.map(
-                        async (songData: any) =>
-                            await getSong(songData.url, true)
-                    )
-                )),
-            };
-        }
+  for (const script of scripts) {
+    let data = JSON.parse(DomUtils.textContent(script));
+    if ("@graph" in data) data = data["@graph"];
+    if (data["@type"] === "MusicAlbum" && !album) {
+      return {
+        artist: data.byArtist.name as string,
+        album: data.name as string,
+      };
     }
+    if (data["@type"] === "MusicAlbum" && album) {
+      let { name, byArtist, tracks } = data;
+
+      return {
+        type: "album",
+        name: name as string,
+        author: byArtist.name as string,
+        tracks: tracks.map((songData: any) => {
+          return {
+            artist: byArtist.name as string,
+            title: songData.name as string,
+          };
+        }),
+      };
+    }
+    if (data["@type"] === "MusicPlaylist" && fast) {
+      let { name, author } = data;
+      return {
+        type: "playlist",
+        name: name as string,
+        author: author.name as string,
+      };
+    }
+    if (data["@type"] === "MusicPlaylist") {
+      let { name, author, track } = data;
+      return {
+        type: "playlist",
+        name: name as string,
+        author: author.name as string,
+        tracks: await Promise.all(
+          track.map(async (songData: any) => await getSong(songData.url, true))
+        ),
+      };
+    }
+  }
 }
 
 export async function fastPlaylist(result: AxiosResponse<string, any>) {
-    let $ = cheerio.load(result.data),
-                    aTitleDivs = $(".songs-list-row__song-name").toArray(),
-                    aArtistDivs = $(".songs-list-row__link").toArray(),
-                    Playlist = [],
-                    i,
-                    j = 0;
+  let $ = cheerio.load(result.data),
+    aTitleDivs = $(".songs-list-row__song-name").toArray(),
+    aArtistDivs = $(".songs-list-row__link").toArray(),
+    Playlist = [],
+    i,
+    j = 0;
 
-    for (i = 0; i < aTitleDivs.length; i++) {
-        Playlist.push({
-            album: (aArtistDivs[j + 2].children[0] as any).data as string,
-            artist: (aArtistDivs[j].children[0] as any).data as string,
-            title: (aTitleDivs[i].children[0] as any).data as string,
-        });
-        j += 3;
-    }
-    return Playlist;
+  for (i = 0; i < aTitleDivs.length; i++) {
+    Playlist.push({
+      album: (aArtistDivs[j + 2].children[0] as any).data as string,
+      artist: (aArtistDivs[j].children[0] as any).data as string,
+      title: (aTitleDivs[i].children[0] as any).data as string,
+    });
+    j += 3;
+  }
+  return Playlist;
 }
 
 /**
  * @param {string} url
  * @returns {Promise<RawAppleSong>}
  */
-export async function getSong(
-    url: string,
-    playlist: boolean = false
+async function getSong(
+  url: string,
+  playlist: boolean = false
 ): Promise<RawAppleSong | undefined> {
-    const result = await axios.get<string>(url);
-    const document = parseDocument(result.data);
-    const { artist, album } = (await findJSONLD(document)) as HalfSong;
-    const regexName = RegExp(/https?:\/\/music\.apple\.com\/.+?\/.+?\/(.+?)\//);
-    const title = regexName.exec(url)?.[1] as string;
+  const result = await axios.get<string>(url);
+  const document = parser.parseDocument(result.data);
+  const { artist, album } = (await findJSONLD(document)) as PartialSong;
+  const regexName = RegExp(/https?:\/\/music\.apple\.com\/.+?\/.+?\/(.+?)\//);
+  const title = regexName.exec(url)?.[1] as string;
 
-    const song: RawAppleSong = {
-        artist,
-        title,
-        album,
-    };
+  const song: RawAppleSong = {
+    artist,
+    title,
+    album,
+  };
 
-    if (playlist === false) {
-        song.type = "song";
-    }
+  if (playlist === false) {
+    song.type = "song";
+  }
 
-    return song;
+  return song;
 }
 
 /**
  * @param {string} url
  * @returns {Promise<?RawApplePlaylist>}
  */
-export async function getPlaylist(
-    url: string
-): Promise<RawApplePlaylist | undefined> {
-    const result = await axios.get<string>(url);
-    const document = parseDocument(result.data);
-    try {
-        const tracks = await fastPlaylist(result)
-        const { type, name, author } = (await findJSONLD(document, true, true)) as HalfPlaylist;
-        return {type, name, author, tracks} as RawApplePlaylist;
-        
-    } catch (error) {
-        return (await findJSONLD(document, true)) as RawApplePlaylist;
-    }
+async function getPlaylist(url: string): Promise<RawApplePlaylist | undefined> {
+  const result = await axios.get<string>(url);
+  const document = parser.parseDocument(result.data);
+  try {
+    const tracks = await fastPlaylist(result);
+    const { type, name, author } = (await findJSONLD(
+      document,
+      true,
+      true
+    )) as PartialPlaylist;
+    return { type, name, author, tracks } as RawApplePlaylist;
+  } catch (error) {
+    return (await findJSONLD(document, true)) as RawApplePlaylist;
+  }
 }
 
 /**
  * @param {string} url
  * @returns {Promise<Promise<RawApplePlaylist|RawAppleSong|undefined>>}
  */
-export async function autoGetApple(
-    url: string
+export async function search(
+  url: string
 ): Promise<RawApplePlaylist | RawAppleSong | undefined> {
-    if (
-        RegExp(/https?:\/\/music\.apple\.com\/.+?\/playlist\//).test(url) ||
-        !url.includes("?i=")
-    ) {
-        return await getPlaylist(url);
-    }
-    if (
-        RegExp(/https?:\/\/music\.apple\.com\/.+?\/album\/.+?\/.+?\?i=/).test(
-            url
-        )
-    ) {
-        return await getSong(url);
-    }
+  if (
+    RegExp(/https?:\/\/music\.apple\.com\/.+?\/playlist\//).test(url) ||
+    !url.includes("?i=")
+  ) {
+    return await getPlaylist(url);
+  }
+  if (
+    RegExp(/https?:\/\/music\.apple\.com\/.+?\/album\/.+?\/.+?\?i=/).test(url)
+  ) {
+    return await getSong(url);
+  }
 }
 
+export default search;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,34 +1,58 @@
-import axios, { AxiosResponse } from "axios";
-import { parseDocument } from "htmlparser2";
-import { getSong, getPlaylist, autoGetApple, fastPlaylist, findJSONLD, HalfPlaylist, RawApplePlaylist } from "../src/index"
+import axios from "axios";
+import parser from "htmlparser2";
+import { findJSONLD, RawApplePlaylist, search } from "../src/index";
 
-test("song", async () =>{
-    await expect(getSong("https://music.apple.com/nz/album/butterfly/1541902791?i=1541903021")).resolves.toMatchSnapshot();
+test("song", async () => {
+  await expect(
+    search("https://music.apple.com/nz/album/butterfly/1541902791?i=1541903021")
+  ).resolves.toMatchSnapshot();
 });
+
 test("album", async () => {
-    await expect(getPlaylist("https://music.apple.com/nz/album/real-growth-takes-time-ep/1541902791")).resolves.toMatchSnapshot();
+  await expect(
+    search(
+      "https://music.apple.com/nz/album/real-growth-takes-time-ep/1541902791"
+    )
+  ).resolves.toMatchSnapshot();
 }, 15000);
+
 test("slowPlaylist", async () => {
-    const playlist = async () => {
-        const result = await axios.get<string>('https://music.apple.com/us/playlist/soda-stereo-essentials/pl.fb224822256d4ef6a7ee4bb2c5a9e37c');
-        const document = parseDocument(result.data);
-        return (await findJSONLD(document, true)) as RawApplePlaylist;
-    };
-    await expect(playlist()).resolves.toMatchSnapshot();
+  const playlist = async () => {
+    const result = await axios.get<string>(
+      "https://music.apple.com/us/playlist/soda-stereo-essentials/pl.fb224822256d4ef6a7ee4bb2c5a9e37c"
+    );
+    const document = parser.parseDocument(result.data);
+    return (await findJSONLD(document, true)) as RawApplePlaylist;
+  };
+  await expect(playlist()).resolves.toMatchSnapshot();
 }, 60000);
 
 test("playlist", async () => {
-    await expect(getPlaylist("https://music.apple.com/us/playlist/soda-stereo-essentials/pl.fb224822256d4ef6a7ee4bb2c5a9e37c")).resolves.toMatchSnapshot();
+  await expect(
+    search(
+      "https://music.apple.com/us/playlist/soda-stereo-essentials/pl.fb224822256d4ef6a7ee4bb2c5a9e37c"
+    )
+  ).resolves.toMatchSnapshot();
 }, 10000);
 
-test("autogetsong", async () =>{
-    await expect(autoGetApple("https://music.apple.com/nz/album/butterfly/1541902791?i=1541903021")).resolves.toMatchSnapshot();
+test("autogetsong", async () => {
+  await expect(
+    search("https://music.apple.com/nz/album/butterfly/1541902791?i=1541903021")
+  ).resolves.toMatchSnapshot();
 });
+
 test("autogetalbum", async () => {
-    await expect(autoGetApple("https://music.apple.com/nz/album/real-growth-takes-time-ep/1541902791")).resolves.toMatchSnapshot();
+  await expect(
+    search(
+      "https://music.apple.com/nz/album/real-growth-takes-time-ep/1541902791"
+    )
+  ).resolves.toMatchSnapshot();
 }, 15000);
+
 test("autogetplaylist", async () => {
-    await expect(autoGetApple("https://music.apple.com/us/playlist/soda-stereo-essentials/pl.fb224822256d4ef6a7ee4bb2c5a9e37c")).resolves.toMatchSnapshot();
+  await expect(
+    search(
+      "https://music.apple.com/us/playlist/soda-stereo-essentials/pl.fb224822256d4ef6a7ee4bb2c5a9e37c"
+    )
+  ).resolves.toMatchSnapshot();
 }, 10000);
-
-
