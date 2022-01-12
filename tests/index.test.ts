7b00b5a3fd02e5550a3b1e8e54ee78a8118c0c7d
Night_Hunter <samuelhuntnz@gmail.com>
2022-01-12 20:46:07 +1300

    added testing

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,22 @@
-import appleMusic from "../src";
+import appleMusic from "../src/index";
 
-const songLink =
-  "https://music.apple.com/us/album/meditations-for-living-an-awakened-life/1602431360";
+const testingTargets = {
+  song: {
+    type: "song",
+    url: "https://music.apple.com/nz/album/butterfly/1541902791?i=1541903021",
+  },
+  album: {
+    type: "album",
+    url: "https://music.apple.com/us/album/meditations-for-living-an-awakened-life/1602431360",
+  },
+  playlist: {
+    type: "playlist",
+    url: "https://music.apple.com/us/playlist/jumpstart/pl.4b0436590393442e9493307577d16742",
+  },
+};
 
-appleMusic(songLink).then(console.log);
+Object.values(testingTargets).forEach((prop) => {
+  test(prop.type, async () => {
+    await expect(appleMusic(prop.url)).resolves.toMatchSnapshot();
+  });
+});
